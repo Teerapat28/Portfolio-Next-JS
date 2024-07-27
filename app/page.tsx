2df5da97fc4cc16306271d6f802c3e9aca129e86
Pooh <poo28kung@gmail.com>
2024-07-27 23:48:02 +0700

    add new component
    recenproject component & update bentogrid

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center 
-    items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main
+      className="relative bg-black-100 flex justify-center 
+    items-center flex-col overflow-hidden mx-auto sm:px-10 px-5"
+    >
       <div className="max-w-full w-full">
-        <FloatingNav navItems={[
-          { name: 'Home', link: '/', icon: <FaHome />}
-        ]}/>
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
         <Grid />
+        <RecentProject />
       </div>
     </main>
   );
